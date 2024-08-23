c023d9089ea81f143b3929dbd957e774db080ad2
Turbobot <turbobot@vercel.com>
2024-08-23 14:06:27 +0530

    feat(create-turbo): apply official-starter transform

--- Desktop/React Projects/travel-list/src/App.js
+++ Desktop/React Projects/travel-list/src/App.js
@@ -1,3 +1,8 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
+
 export default function App() {
   return (
     <div className="app">
@@ -22,13 +27,32 @@ function Form() {
 }
 
 function PackingList() {
-  return <div className="list">LIST</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.description} {item.quantity}
+      </span>
+      <button></button>
+    </li>
+  );
 }
 
 function Stats() {
   return (
     <footer className="stats">
-      <em>🤷‍♂️ You have X items in your list, and you already packed X </em>
+      <em>🤷‍♂️You have X items in your list, and you already packed X </em>
     </footer>
   );
 }
